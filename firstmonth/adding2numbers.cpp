005a497fd3f8d54d8b8b7de57dc3765290088e13
Narayan G V <narayanvadhyar@gmail.com>
2025-10-01 16:42:04 +0530

    sum of 2 numbers

---
+++ firstmonth/adding2numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of first number"<<endl;
+    cin>>a;
+    cout<<"Enter the value of second number"<<endl;
+    cin>>b;
+    int sum=a+b;
+    cout<<"The sum of the entered numbers is  "<<sum;
+}
